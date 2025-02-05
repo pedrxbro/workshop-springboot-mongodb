4a548896097dc29ed405953fc82453fdf612361d
pedrxbro <pedrohdpaula@edu.univali.br>
2025-02-04 21:36:26 -0300

    Inserção de usuário com POST

--- src/main/java/com/univali/workshopmongo/services/UserService.java
+++ src/main/java/com/univali/workshopmongo/services/UserService.java
@@ -1,6 +1,7 @@
 package com.univali.workshopmongo.services;
 
 import com.univali.workshopmongo.domain.User;
+import com.univali.workshopmongo.dto.UserDTO;
 import com.univali.workshopmongo.repositories.UserRepository;
 import com.univali.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,4 +25,13 @@ public class UserService {
         }
         return user;
     }
+
+    public User insert(User obj){
+        return repo.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDto){
+        return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+    }
+
  }
