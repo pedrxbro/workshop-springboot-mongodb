4b767c6bc93a74bff3a5d1695d2cb8ce45dd2618
pedrxbro <pedrohdpaula@edu.univali.br>
2025-03-02 10:49:35 -0300

    COnsulta simples com @Query

--- src/main/java/com/univali/workshopmongo/repositories/PostRepository.java
+++ src/main/java/com/univali/workshopmongo/repositories/PostRepository.java
@@ -1,13 +1,16 @@
 package com.univali.workshopmongo.repositories;
 
 import com.univali.workshopmongo.domain.Post;
-import com.univali.workshopmongo.domain.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+
+    @Query("{ title: { $regex: ?0, $options: 'i' } }")
+    List<Post> searchTitle(String title);
     List<Post> findByTitleContainingIgnoreCase(String title);
 }
